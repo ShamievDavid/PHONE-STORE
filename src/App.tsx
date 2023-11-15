3e118c5779145cec6fbf2a3affbc9d38a4452038
david.shamiiev <david.shamiiev@rad4m.com>
2023-11-15 14:07:44 +0200

    clone

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Outlet } from 'react-router-dom';
+import { ProductsProvider } from 'context/productsContext';
+import { CartProvider } from 'context/cartContext';
+import { Footer, Header } from './components';
+import './App.scss';
+
+const App = () => (
+  <ProductsProvider>
+    <CartProvider>
+      <div className="app">
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+    </CartProvider>
+  </ProductsProvider>
+);
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import './style/main.scss';
+import { Root } from './routing';
+
+ReactDOM.render(
+  <Root />,
+  document.getElementById('root'),
+);
